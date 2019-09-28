461fd492472337add30d854703ac96a3344046fe
Berkay Aydin <sbayd06@gmail.com>
2019-09-28 15:37:11 +0300

    perf

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,43 @@ import React from 'react';
 import ReactCV from 'react-cv';
 import Helmet from 'react-helmet';
 import { CVData } from '../CVData';
+import '../styles.css';
 
-export default () => (
-  <>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>Sabri Berkay Aydin Online Resume</title>
-      <meta name="Description" CONTENT="Sabri Berkay Aydin, Lead Software Developer. Online CV / Resume " />
-    </Helmet>
-    <ReactCV
-      {...CVData}
-    />
-    <div>You can create your own CV like this</div>
-  </>
-);
+export default class MYCV extends React.Component {
+  componentDidMount() {
+    this.removeSVGHack();
+  }
+
+  componentDidUpdate() {
+    this.removeSVGHack();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  removeSVGHack() {
+    try {
+      const svgHack = global.document.querySelector('#svg_hack');
+      if (svgHack) {
+        svgHack.innerHTML = '';
+        global.document.head.removeChild(svgHack);
+      }
+    } catch (e) {
+      console.error('shit', e);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Sabri Berkay Aydin Online Resume</title>
+          <style id="svg_hack" dangerouslySetInnerHTML="svg { display: none }" />
+          <meta name="Description" CONTENT="Sabri Berkay Aydin, Lead Software Developer. Online CV / Resume " />
+        </Helmet>
+        <ReactCV
+          {...CVData}
+        />
+      </>
+    );
+  }
+}
