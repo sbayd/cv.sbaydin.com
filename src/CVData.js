ed8dcbe0ce40281698750425d9a91f73ea543d59
Berkay Aydın <sbayd06@gmail.com>
2023-11-12 22:13:58 +0300

    feat: update cv data

--- src/CVData.js
+++ src/CVData.js
@@ -1,9 +1,9 @@
 import Profile from './profile.jpeg';
 
-export const CVData = {
+const CVData = {
   personalData: {
     name: 'S.Berkay Aydın',
-    title: 'Senior Software Developer',
+    title: 'Engineering Director & Innovator in Tech',
     image: Profile,
     contacts: [
       { type: 'email', value: 'sbaydin06@gmail.com' },
@@ -31,54 +31,62 @@ export const CVData = {
     {
       type: 'text',
       title: 'Career Profile',
-      content: "I'm an Engineering Director and Frontend Architect at Jotform with a lifelong passion for coding that began at the age of 13.\n\nIn my role, I'm known for a 'Get things done' attitude. I lead technical discussions, create solutions, manage hiring, and mentor teams. I've successfully led web and mobile app development projects, each reaching millions of users. Additionally, I manage a complex monorepo with 400+ packages, emphasizing system efficiency and automation.\n\nBeyond work, I actively engage in side projects like 'Tefez,' an online appointment scheduler, and 'Dream Atlas Mobile' built with React Native. My commitment to innovation drives me to continually explore new technologies and methodologies.\n\nI'm also dedicated to mentorship, offering guidance both in the workplace and through non-profit organizations and paid lessons.",
+      content: '**Engineering Director and Frontend Architect at Jotform**, my journey in technology began at 13. Known for a decisive **"Get things done"** approach, I have led significant web and mobile app projects, each reaching millions. Experienced in **backend, frontend, desktop, and mobile development**, I\'ve consistently pushed innovation\'s boundaries. Evolving from hands-on developer to visionary leader, I remain deeply engaged in product creation.\n\n&nbsp;\n\nAt Jotform, along with leading technical discussions, creating innovative solutions, overseeing hiring, and mentoring teams, I manage a complex monorepo of **400+ packages**, focusing on system efficiency and scalability.\n\n&nbsp;\n\nBeyond Jotform, I offer **consulting services** and mentorship, and have launched AI powered projects like [**Tefez**](tefez.com) and [**Dream Atlas**](https://apps.apple.com/th/app/dream-atlas-analyze-dreams/id6470039596). My career is a testament to continuous learning and leadership in the fast-paced tech industry.',
       icon: 'usertie',
     },
     {
       type: 'experiences-list',
-      title: 'Experiences',
+      title: 'Career Journey',
       description: '',
       icon: 'archive',
       items: [
+
         {
           title: 'Engineering Director / Frontend Architect',
           company: 'JotForm',
-          description: "In my role as Engineering Director and Frontend Architect at Jotform, I lead technical discussions, create common solutions, manage hiring processes, and mentor teams. I've successfully led web and mobile app development projects, each reaching millions of users. My responsibilities include managing a complex monorepo with 400+ packages, emphasizing system efficiency and automation.",
+          description: "In my dual role at Jotform, I lead technical discussions, devise innovative solutions, and oversee hiring and team mentorship. I've spearheaded major web and mobile app projects, reaching millions of users, and manage a monorepo with 400+ packages, focusing on system efficiency and automation.",
           companyWebSite: 'http://jotform.com',
-          companyMeta: '',
           datesBetween: '2022.10 - Present',
-          descriptionTags: ['Engineering Management', 'Technical Leadership', 'System Efficiency'],
         },
         {
           title: 'Front-end Architect',
           company: 'JotForm',
-          description: "As a member of the Front-end Architecture team at Jotform, I am committed to enhancing developer efficiency and job satisfaction. My primary responsibilities include creating shared solutions/components for development teams, mentoring junior developers, transitioning our polyrepo stack to a monorepo, and maintaining and updating our development environments.",
+          description: 'As part of the Front-end Architecture team, I focus on enhancing developer efficiency and satisfaction. Responsibilities include creating shared solutions for teams, mentoring, transitioning to a monorepo, and maintaining development environments.',
           companyWebSite: 'http://jotform.com',
-          companyMeta: '',
           datesBetween: '2021.10 - Present',
-          descriptionTags: ['Javascript', 'React', 'Architect', 'Mentorship'],
         },
         {
           title: 'Team Lead / Senior Front-end Developer',
           company: 'JotForm',
-          description: 'In my capacity as the Team Lead & Senior Front-end Developer within JotForm Source Team, I managed a cross-functional production team, mentored junior developers, created clean and shareable common components, and predominantly worked on the product itself, catering to an extensive user base of over 10 million users.',
+          description: 'Managed a cross-functional team, mentored junior developers, and developed common components, significantly contributing to products used by over 10 million users.',
           companyWebSite: 'http://jotform.com',
-          companyMeta: '',
           datesBetween: '2017.10 - 2021.10',
-          descriptionTags: ['Javascript', 'React', 'Team Management'],
         },
         {
           title: 'Front-end Developer',
           company: 'JotForm',
-          description: 'At JotForm, I utilized Javascript, predominantly with ReactJS, and functioned as a front-end developer, contributing to the development of new features.',
+          description: "Focused on Javascript, mainly with ReactJS, contributing to the development of new features and enhancements in JotForm's offerings.",
           companyWebSite: 'http://jotform.com',
-          companyMeta: '',
           datesBetween: '2016.8 - 2017.10',
+        },
+        {
+          title: 'Co-founder',
+          company: 'Tefez',
+          description: 'Co-founded and developed Tefez, an AI-powered online appointment scheduling tool, contributing to its growth and technological advancement.',
+          companyWebSite: 'http://tefez.com',
+          datesBetween: '2021 - Present',
+        },
+        {
+          title: 'Creator',
+          company: 'Dream Atlas',
+          description: 'Developed Dream Atlas, an innovative AI-based dream analysis app available on App Store and Google Play, showcasing my skills in mobile app development and AI integration.',
+          companyWebSite: 'https://apps.apple.com/th/app/dream-atlas-analyze-dreams/id6470039596',
+          datesBetween: '2023 - Present',
         },
         {
           title: 'Full-Stack Developer',
           company: 'YD Software',
-          description: 'During my tenure at YD Software, I employed the ASP.Net MVC Framework with N-Tier Architecture to develop management systems, learning management systems, and big data projects. Additionally, I engaged in Android and iOS application development using Xamarin and NDK.',
+          description: 'Employed ASP.Net MVC and N-Tier Architecture for system development, including management and learning systems, and engaged in Android and iOS app development using Xamarin and NDK.',
           companyWebSite: 'http://ydyazilim.com',
           companyMeta: 'ANKARA METU Technopolis (ODTU Teknokent)',
           datesBetween: '2012 - 2016.8',
@@ -86,7 +94,7 @@ export const CVData = {
         {
           title: 'Intern',
           company: 'YD Software',
-          description: 'As an intern, I gained practical experience working on a Learning Management System, encompassing both desktop and web applications.',
+          description: 'Gained hands-on experience in Learning Management System development, working on both desktop and web applications.',
           companyWebSite: 'http://ydyazilim.com',
           companyMeta: 'ANKARA METU Technopolis (ODTU Teknokent)',
           datesBetween: '2012.06 - 2012.10',
@@ -95,9 +103,10 @@ export const CVData = {
     },
     {
       type: 'projects-list',
-      title: 'Projects',
-      description: 'Below, you will find a selection of featured projects. For more detailed information, please do not hesitate to contact me.',
+      title: 'Featured Projects',
+      description: 'Discover my selection of key projects that highlight innovation and technical skill. For more details, please contact me.',
       icon: 'tasks',
+
       groups: [
         {
           sectionHeader: 'JotForm',
@@ -106,27 +115,27 @@ export const CVData = {
             {
               title: 'JotForm Approvals / JotForm Workflow',
               projectUrl: 'https://www.jotform.com/products/approvals/',
-              description: 'A canvas-based drag & drop process builder, this project represents one of my latest and most challenging endeavors. It is constructed using React, Redux, Redux-Saga, and incorporates numerous sophisticated patterns and technologies.',
+              description: 'A sophisticated drag & drop process builder using React, Redux, Redux-Saga. This project is a testament to my commitment to creating innovative, user-centric solutions.',
             },
             {
               title: 'JotForm Mobile Forms',
               projectUrl: 'https://www.jotform.com/products/mobile-forms/',
-              description: 'A mobile app enabling users to create forms and collect submissions. With over 1 million downloads, I assumed the roles of Team Lead and Lead Developer, utilizing React Native, Redux, and other cutting-edge technologies.',
+              description: 'A pivotal mobile application enabling form creation and submission, boasting over 1 million downloads, highlighting my role in its development with React Native and Redux.',
             },
             {
-              title: 'PDF Editor',
+              title: 'Jotform PDF Editor',
               projectUrl: 'https://www.jotform.com/products/pdf-editor/',
-              description: 'A PDF Editor empowering users to create customized PDFs or fillable forms. I served as the Lead Javascript Developer, employing React and Redux.',
+              description: 'Leading the development of a user-friendly PDF Editor, I utilized React & NodeJS to empower users with customized PDF creation capabilities.',
             },
             {
               title: 'JotForm Cards',
               projectUrl: 'https://www.jotform.com/cards/',
-              description: 'Introducing the next-generation form type, JotForm Cards boasts over 1 million users. My role as a Javascript Developer involved working with React, Server-Side Rendering (SSR), Vanilla JS, and Webpack.',
+              description: 'A next-gen form solution with over a million users, where I contributed as a Javascript Developer, enhancing the user experience with React and SSR.',
             },
             {
               title: 'Easiest Online Form Builder',
               projectUrl: 'https://www.jotform.com/meet-jotform-4/',
-              description: 'Renowned as the world\'s most powerful WYSWYG form builder, this project challenged me as a Javascript Developer, utilizing React, Redux, and Socket.io.',
+              description: 'As a Javascript Developer, I played a crucial role in developing this powerful WYSWYG form builder, integrating React, Redux, and Socket.io.',
             },
           ],
         },
@@ -136,55 +145,50 @@ export const CVData = {
           items: [
             {
               title: 'Odtu Teknokent Internal Systems (TKBS - YFYI - GGJ - BIGG - ATOM - TeknoJump)',
-              projectUrl: 'http:////giris.odtuteknokent.com.tr',
-              description: 'I have worked on multiple projects for ODTU Teknokent\'s (METU Technopolis) internal workflow. ( C# / ASP.NET MVC / SQL Server / Xamarin / MongoDB / Web Services)',
+              projectUrl: 'http://giris.odtuteknokent.com.tr',
+              description: "Contributed to ODTU Teknokent's internal systems, employing diverse technologies like C#, ASP.NET MVC, and Xamarin to enhance operational efficiency.",
             },
-          ]
+          ],
         },
         {
           sectionHeader: 'Side Projects',
-          description: 'Explore some of my recent side projects below.',
+          description: 'Explore some of my innovative side projects below.',
           items: [
             {
               title: 'Tefez - Online Appointment Scheduler',
               projectUrl: 'https://tefez.com',
-              description: 'Tefez simplifies the process of scheduling appointments, streamlining and enhancing booking experiences.',
+              description: 'Co-founded Tefez, an AI-powered tool revolutionizing appointment scheduling, exemplifying my ability to merge technology with practical solutions.',
             },
             {
               title: 'Dream Atlas Mobile - Dream Journal App',
-              projectUrl: 'https://play.google.com/store/apps/details?id=com.sbayd.dreamatlas',
-              description: 'Dream Atlas Mobile serves as a dream journal app, designed for Android devices and available on Google Play.',
+              projectUrl: 'https://apps.apple.com/th/app/dream-atlas-analyze-dreams/id6470039596',
+              description: 'Developed an AI-based dream analysis app for Android and iOS, showcasing my skill in integrating advanced technology with user-friendly app design.',
             },
             {
               title: 'Soldd - Online Marketplace (Mobile APP)',
               projectUrl: 'https://play.google.com/store/apps/details?id=com.soldd.app',
-              description: 'Soldd is an online marketplace for Turkish users, available on both iOS and Android. I utilized React Native, React, and GraphQL to bring this project to life.',
+              description: 'Created Soldd, a versatile marketplace app for Turkish users, leveraging React Native and GraphQL for an optimal user experience.',
             },
             {
               title: 'React-CV - Open Source',
               projectUrl: 'https://github.com/sbayd/react-cv',
-              description: 'As a fervent supporter of open source, I contributed to the community by creating React-CV, a tool that enables the creation of CV websites similar to this one!',
+              description: 'Developed React-CV, an open-source tool for building CV websites, contributing to the developer community with my expertise.',
             },
             {
               title: 'React-Flippy - Open Source',
               projectUrl: 'https://github.com/sbayd/react-flippy',
-              description: 'React-Flippy, an open-source project, introduces a React component for flipping cards. Despite its age, it remains a popular choice among developers.',
+              description: 'React-Flippy, a popular open-source React component for flipping cards, showcases my commitment to sharing innovative solutions.',
             },
           ],
         },
       ],
+
     },
     {
       type: 'common-list',
       title: 'Education',
       icon: 'graduation',
       items: [
-        {
-          title: 'Entrepreneurship & Innovation (MS)',
-          authority: 'Anadolu University',
-          authorityWebSite: 'https://anadolu.edu.tr',
-          rightSide: '2021 - Present',
-        },
         {
           title: 'Management Information Systems (BS)',
           authority: 'Anadolu University',
@@ -194,15 +198,14 @@ export const CVData = {
       ],
     },
     {
-
       type: 'common-list',
       title: 'Conferences & Certificates',
-      description: '',
+      description: 'My engagement in key conferences and pursuit of certifications underscores my commitment to continual learning and staying current in the IT field. These experiences have enriched my professional journey, providing opportunities for knowledge expansion, networking, and sharing insights.',
       icon: 'comments',
       items: [
         {
-          title: 'O\'Reilly Software Architecture Conference 2019 (SACON) / Berlin, Germany',
-          authority: 'O\'Reilly',
+          title: "O'Reilly Software Architecture Conference 2019 (SACON) / Berlin, Germany",
+          authority: "O'Reilly",
           authorityMeta: 'Attendee',
           authorityWebSite: 'https://www.oreilly.com/conferences/software-architecture.html',
         },
@@ -256,15 +259,35 @@ export const CVData = {
     },
     {
       type: 'tag-list',
-      title: 'Skills Proficiency',
+      title: 'Core Competencies',
       icon: 'rocket',
-      items: ['Javascript', 'React', 'Redux', 'React Native', 'NPM', 'PNPM', 'YARN', 'Git', 'Webpack', 'GraphQL', 'C#', 'Entity Framework', 'SQL', 'PHP', 'Software Craftmanship', 'SOLID Principles', 'Team Leading', 'Front-end Development', 'Web Development'],
+      items: [
+        'Strategic Technology Leadership',
+        'Advanced JavaScript & React Development',
+        'Scalable System Architecture',
+        'Efficient Project Management',
+        'Innovative Product Development',
+        'Cross-Platform Solutions (Web & Mobile)',
+        'AI Fundamentals & Application',
+        'DevOps & Automation Strategies',
+        'Mentorship & Team Building',
+      ],
     },
     {
       type: 'tag-list',
-      title: 'Hobies & Interests',
+      title: 'Hobbies & Interests',
       icon: 'cubes',
-      items: ['Pipes', 'Analog Photography', 'Cooking', 'Literature History'],
+      items: [
+        'Technology Innovation & Trends',
+        'Open-Source Contributions',
+        'Exploring AI Applications',
+        'Digital & Analog Photography',
+        'Culinary Arts & Experimentation',
+        'Historical & Tech Literature',
+        'Outdoor Activities & Nature Exploration',
+      ],
     },
   ],
 };
+
+export default CVData;
